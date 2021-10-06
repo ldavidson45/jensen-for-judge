c9964c2155ba5f469328fc0496461f432033231e
Liz Davidson <lizdavidson45@gmail.com>
2021-10-06 19:48:04 -0400

    Add deploy script

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
 
+  router: {
+    base: '/jensen-for-judge/',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Linda Jensen for Juvenile Court Judge',
